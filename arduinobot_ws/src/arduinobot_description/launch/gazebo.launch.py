6c063c6af61be6895be066507f5106ab93516b0a
Andres <andresaleman142@gmail.com>
2025-10-24 09:50:15 -0600

    feat: :rocket: MoveIt Integration with launch file

--- arduinobot_ws/src/arduinobot_description/launch/gazebo.launch.py
+++ arduinobot_ws/src/arduinobot_description/launch/gazebo.launch.py
@@ -53,7 +53,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py")),
         launch_arguments=[
-            ("gz_args", f"-v 4 -r {world_path} {physics_engine}")
+            ("gz_args", f"-v 4 -r {world_path}")
         ]
     )
 
